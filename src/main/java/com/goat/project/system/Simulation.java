ee3398e3b69e0c3f169151f6fc31f0572712ea0e
cheve <cheve@desktop-hqintd8.tail8ab5ad.ts.net.>
2026-01-18 18:46:55 +0800

    test

--- src/main/java/com/goat/project/system/Simulation.java
+++ src/main/java/com/goat/project/system/Simulation.java
@@ -12,7 +12,7 @@ public class Simulation {
         String adminPass = "admin123";
 
         String[] cashierUsers = {"cashier1", "cashier2"};
-        String[] cashierPass = {"cashier12", "cashier45"};
+        String[] cashierPass = {"cashier123", "cashier456"};
 
         int attempts = 0;
 
