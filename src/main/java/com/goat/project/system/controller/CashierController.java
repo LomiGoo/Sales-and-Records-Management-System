8b4e08dab4d26f93ddb8774da800e6fa3326e062
cheve <cheve@desktop-hqintd8.tail8ab5ad.ts.net.>
2026-01-18 15:29:43 +0800

    code point 1

--- src/main/java/com/goat/project/system/controller/CashierController.java
+++ src/main/java/com/goat/project/system/controller/CashierController.java
@@ -1,5 +1,33 @@
 package com.goat.project.system.controller;
 
+import java.util.ArrayList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class CashierController {
+    
+    // =======================
+    // Category Labels
+    // =======================
+    @FXML
+    private Label dummyCateg;
+    private ArrayList<String> category = new ArrayList<>();
+    
+    @SuppressWarnings("unused")
+    private void initialize() {
+        category.add("Main Dish");
+        category.add("Drinks");
+        category.add("Fried");
+        
+        generateNewCategory();
+    }
+    
+    int count = 1;
+    public void generateNewCategory() {
+        for(String categ : category) {
+            Label newLabel = new Label(count + ". " + categ.indexOf(categ));
+            System.out.println(categ.indexOf(categ));
+        }
+    }
     
 }
